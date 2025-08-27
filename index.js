04da1f31f1bc60736996069223338a09cc62a543
Mohammed_Qassem <mohqasem02@gmail.com>
2025-08-28 00:22:30 +0400

    feat(posts): add homepage route to display all blog posts feat(posts): implement create post form and submission feat(routes): add post creation flow with redirect to homepage

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-import express from 'express'; 
+import { render } from "ejs";
+import express from "express";
 
-const app = express(); 
-const port = 4000; 
-app.use(express.static('public')); 
+const app = express();
+const port = 4000;
+app.use(express.static("public"));
 
-app.get('/', (req, res, next) => {
-    res.render('index.ejs'); 
-}); 
+// Use the urlencoded middleware
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/create-post', (req, res, next) => {
-    consol
-}); 
+//render the home page
+app.get("/", (req, res, next) => {
+  res.render("index.ejs");
+});
 
-app.listen(port, ()=> {
-    console.log(`Server is running at port ${port}`); 
-}); 
+// render the create post file
+app.get("/create-post", (req, res, next) => {
+  res.render("partials/createpost.ejs"); // Remove leading slash and use correct filename
+});
+
+app.post("/", (req, res, next) => {
+  const article = req.body.article;
+  const title = req.body.title; 
+  console.log(title);
+  console.log(article);
+  res.render("index.ejs");
+});
+
+// Run the server
+app.listen(port, () => {
+  console.log(`Server is running at port ${port}`);
+});
